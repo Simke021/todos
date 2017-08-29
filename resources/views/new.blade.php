0a926cf2ca4ab0bbe034235e9337f9453e504e03
Aleksandar Ljubišić <simke20@yahoo.com>
2017-08-29 13:05:24 +0200

    todos controller and database seed with faker data for todo

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    New Page
                 </div>
             </div>
         </div>
